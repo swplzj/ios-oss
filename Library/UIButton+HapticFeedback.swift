2ed2de2b314eed55eef860f5d30c488bdc3dcbdd
Justin Swart <justinswart@users.noreply.github.com>
2018-11-30 14:20:22 -0800

    Set minimum deployment target to iOS 10.3, remove availability checks

--- Library/UIButton+HapticFeedback.swift
+++ Library/UIButton+HapticFeedback.swift
@@ -2,14 +2,10 @@ import UIKit
 
 extension UIButton {
   public func generateSelectionFeedback() {
-    if #available(iOS 10.0, *) {
-      UIFeedbackGenerator.ksr_selection()
-    }
+    UIFeedbackGenerator.ksr_selection()
   }
 
   public func generateSuccessFeedback() {
-    if #available(iOS 10.0, *) {
-      UIFeedbackGenerator.ksr_success()
-    }
+    UIFeedbackGenerator.ksr_success()
   }
 }
